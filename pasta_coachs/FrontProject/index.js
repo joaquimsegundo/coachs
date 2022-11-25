bbe1098528d6f75ed1efbad45c292eb85dc50baa
jcmsegnd <info.joaquimsegundo@gmail.com>
2022-11-24 22:33:14 -0300

    algumas coisas

--- pasta_coachs/FrontProject/index.js
+++ pasta_coachs/FrontProject/index.js
@@ -1,1 +1,13 @@
 import axios from 'axios';
+
+let url = "http://localhost:3001/users";
+const configs = {method: 'get', url: url};
+
+let resp = await axios(configs);
+console.log(resp.data)
+
+
+
+
+
+
